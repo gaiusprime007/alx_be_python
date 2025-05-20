5f58d21c4a1628af5c6877ed43fbf30e8470486c
Gaius <gaiusdeveloper@gmail.com>
2025-05-20 15:32:08 +0000

    Create basic_operations.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,22 @@
+#Create a file named basic_operations.py.
+#In this file, you will define two variables: number1 and number2, with the values 10 and 5, respectively.
+#You do not need to write any functions or import any modules.
+#Calculate the sum, difference (by subtracting number2 from number1), and product of number1 and number2.
+#Print the results of each operation in the format: [operation] of [number1] and [number2] is [result].
+
+number1 = 10
+number2 = 5
+
+#Additon of number1 and number 2
+sum = number1 + number2
+
+#Subraction 
+differnce = number2 - number1
+
+#Multiplication
+product = number1 * number2
+
+##Display results
+print("The sum of ", number1 ,"and ",  number2, 'is', sum )
+print("The difference of ", number1 ,"and ",  number2, 'is ', difference )
+print("The product of ", number1 ,"and ",  number2, 'is', product )
