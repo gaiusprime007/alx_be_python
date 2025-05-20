213831a3f18a063d357bbfa65109404049a4b2c4
Gaius <gaiusdeveloper@gmail.com>
2025-05-20 16:17:26 +0000

    Bug fixes

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,10 @@
 ##Finance calculator
+
 monthly_income = float(input("Enter your monthly income: "))
 monthly_expenses = float(input("Enter your total monthly expenses: "))
 
 #calculate monthly savings
-monthly_savings = float(monthly_income - monthly_expenses)
+monthly_savings = monthly_income - monthly_expenses
 
 
 #Calculate future value of savings
