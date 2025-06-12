efd3d3eed75386c970ad4f2666e7c74d0b9c9240
Gaius <gaiusdeveloper@gmail.com>
2025-06-12 14:22:05 +0000

    Update temp_conversion_tool.py
    indentation fixes

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,28 +2,33 @@ FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
 def convert_to_celcius(fahrenheit):
-     celcius_temp = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
-     return celcius_temp
+    celcius_temp = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+    return celcius_temp
 
 
 def convert_to_fahrenheit(celcius):
-     fahrenheit_temp = (celcius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
-     return fahrenheit_temp
+    fahrenheit_temp = (celcius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+    return fahrenheit_temp
 
 
 def main():
-     temp = float(input("Enter the temperature to convert: "))
-     unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+    try:
+        temp = float(input("Enter the temperature to convert: "))
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
 
-     if unit == 'F':
-          result = convert_to_celcius(temp)
-          print(f"{temp}°F is {result}°C")
-     elif unit == 'C':
-          result = convert_to_fahrenheit(temp)
-          print(f"{temp}°C is {result}°F")
+        if unit == 'F':
+            result = convert_to_celcius(temp)
+            print(f"{temp}°F is {result}°C")
+        elif unit == 'C':
+            result = convert_to_fahrenheit(temp)
+            print(f"{temp}°C is {result}°F")
 
-     else:
-          print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+        else:
+            print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
+    except ValueError:
+        print("Invalid temperature. Please enter a numeric value. ")
+
 
 if __name__ == "__main__":
-     main()
+    main()
