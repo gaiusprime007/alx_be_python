40bf612bef8e1f79b5fc3c856489fdbf8f36dd4b
Gaius <gaiusdeveloper@gmail.com>
2025-06-15 22:49:04 +0000

    Create test_simple_calculator.py

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,36 @@
+# test_simple_calculator.py
+
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+
+    def setUp(self):
+        """Create a new calculator instance before each test."""
+        self.calc = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(3, 5), -2)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(4, 2), 8)
+        self.assertEqual(self.calc.multiply(0, 5), 0)
+        self.assertEqual(self.calc.multiply(-3, 3), -9)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(-9, 3), -3)
+        self.assertEqual(self.calc.divide(0, 1), 0)
+
+    def test_divide_by_zero(self):
+        self.assertIsNone(self.calc.divide(5, 0))  # Should return None
+
+if __name__ == '__main__':
+    unittest.main()
