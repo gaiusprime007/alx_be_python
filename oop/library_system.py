1c8fd1d27280421a13f5eb9e4865409d41fa1d8f
Gaius <gaiusdeveloper@gmail.com>
2025-06-22 21:06:47 +0000

    Create library_system.py

---
+++ oop/library_system.py
@@ -0,0 +1,45 @@
+# Book base class
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+
+# EBook subclass
+class EBook(Book):
+    def __init__(self, title, author, file_size):
+        super().__init__(title, author)
+        self.file_size = file_size
+
+    def __str__(self):
+        return f"{super().__str__()} [E-Book, {self.file_size}MB]"
+
+# PrintBook subclass
+class PrintBook(Book):
+    def __init__(self, title, author, page_count):
+        super().__init__(title, author)  
+        self.page_count = page_count
+
+    def __str__(self):
+        return f"{super().__str__()} [Print, {self.page_count} pages]"
+
+# Library composition class
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only instances of Book or its subclasses can be added.")
+
+    def list_books(self):
+        if not self.books:
+            print("No books in the library.")
+        else:
+            for book in self.books:
+                print(book)
+
